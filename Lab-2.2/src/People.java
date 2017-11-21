189218b0a1afd9888af29933bf22d8050c7c91ce
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-11-21 09:25:41 -0500

    asdf

--- Lab-2.2/src/People.java
+++ Lab-2.2/src/People.java
@@ -1,5 +1,51 @@
 
-public class People {
+public abstract class People 
+{
+	private String firstname, familyname;
+	private double height;
+	private int weight;
+	public People(String firstname, String familyname, double height, int weight)
+	{
+		this.firstname = firstname;
+		this.familyname = familyname;
+		this.height = height;
+		this.weight = weight;
+	}
+	public String getFirstname() 
+	{
+		return firstname;
+	}
 
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getFamilyname() {
+		return familyname;
+	}
+
+	public void setFamilyname(String familyname) {
+		this.familyname = familyname;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+
+	
 }
- 4
+
+
---
+++ Lab-2.2/src/Runner.java
@@ -0,0 +1,6 @@
+
+public class Runner 
+{
+	static String[] firstNames = {"Darren, Kyle, Andrew, Leo, Arthur"};
+	static String[} lastNames = {"Jonhson, Doe, Karling, Hapsburg, Babenger"};
+}
