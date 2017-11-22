19f276d1b6c155c9416b1fe4201dbca03ee1b5d5
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-22 09:26:16 -0500

    d

--- Lab-2.2/src/Runner.java
+++ Lab-2.2/src/Runner.java
@@ -1,6 +1,16 @@
 
 public class Runner 
 {
-	static String[] firstNames = {"Darren, Kyle, Andrew, Leo, Arthur"};
-	static String[} lastNames = {"Jonhson, Doe, Karling, Hapsburg, Babenger"};
+	static String[] firstNames = {"Darren, Kyle, Andrew, Leo, Arthur, Charles, Louis, Henry, Gustav"};
+	static String[] lastNames = {"Jonhson, Doe, Karling, Hapsburg, Babenger, Vasa, Wettin, Salian, Capet"};
+	
+	public static void main(String[] args)
+	{
+		public abstract Teacher;
+		public abstract Students;
+		public abstract Classroom;
+	}
+	
+	
+	
 }
---
+++ Lab-2.2/src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student extends People
+{
+	private double gpa;
+	private double osis;
+	private String major;
+	public Student(double gpa, double osis, String major)
+	{
+		this.gpa = gpa;
+	}
+}
