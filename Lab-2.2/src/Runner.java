b2ab4e7d9bca9c6abf1e8c5a51f327fa080af3db
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-11-29 09:26:16 -0500

    344

--- Lab-2.2/src/Runner.java
+++ Lab-2.2/src/Runner.java
@@ -3,13 +3,13 @@ public class Runner
 {
 	static String[] firstNames = {"Darren, Kyle, Andrew, Leo, Arthur, Charles, Louis, Henry, Gustav"};
 	static String[] lastNames = {"Jonhson, Doe, Karling, Hapsburg, Babenger, Vasa, Wettin, Salian, Capet"};
+	static double[] heights = {60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76};
+	static double[] weight = {150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200};
+	static double[] GPA = {
 	
 	public static void main(String[] args)
 	{
 		private String Teacher;
 	}
-	
-	s
-	
 }                                                
                                                    
--- Lab-2.2/src/Student.java
+++ Lab-2.2/src/Student.java
@@ -1,7 +1,6 @@
 
 public class Student extends People
 {
-
 	public Student(String firstname, String familyname, double height, int weight, double GPA, double osis, String major) 
 	{
 		super(firstname, familyname, height, weight);
@@ -9,7 +8,7 @@ public class Student extends People
 	}
 	public double GPA()
 	{
-		s
+		for (Person p : this.student)
 	}
 	
 	
