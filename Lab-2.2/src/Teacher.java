0c3ebb528edad296006ed66a1e47261da4195405
Student <Student@10.148.34.229>
2017-12-01 11:01:57 -0500

    3

--- Lab-2.2/src/Teacher.java
+++ Lab-2.2/src/Teacher.java
@@ -1,17 +1,17 @@
 
 public class Teacher extends People
 {
-	private String subject;
-	private String title;
 	public Teacher(String firstname, String familyname, double height, int weight, String subject, String title) 
 	{
 		super(firstname, familyname, height, weight);
 	}
 	
-	public String subject() {
+	public String subject() 
+	{
 		return subject();
 	}
-	public String title() {
+	public String title() 
+	{
 		return title();
 	}
 
